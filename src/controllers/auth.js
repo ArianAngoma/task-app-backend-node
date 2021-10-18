95f38290f370eaa364b54e85f645f1c4d69ad876
unknown <arian.angoma@tecsup.edu.pe>
2021-10-18 03:35:18 -0500

    Crear nuevo usuario y guardarla en DB

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,25 @@
-const createUser = (req, res) => {
-    console.log(req.body);
-    res.json({
-        msg: 'ok'
-    });
+/* Importaciones propias */
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const user = new User({name, email, password});
+        /* Guardar en DB */
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            user
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el Administrador'
+        });
+    }
 }
 
 module.exports = {
