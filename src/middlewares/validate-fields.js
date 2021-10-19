8c6e35c25b6ce1ca75d684fe7d84ebb52d4842de
unknown <arian.angoma@tecsup.edu.pe>
2021-10-18 21:39:34 -0500

    Respuesta de errores cambiada

--- src/middlewares/validate-fields.js
+++ src/middlewares/validate-fields.js
@@ -5,7 +5,8 @@ const validateFields = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({
         ok: false,
-        errors: errors.mapped()
+        // errors: errors.mapped()
+        errors: errors.array()
     });
 
     next();
