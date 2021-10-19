d47e6e359d96de472f51a23aa6a069c2aeef7379
unknown <arian.angoma@tecsup.edu.pe>
2021-10-19 01:34:36 -0500

    Endpoint para crear nuevo proyecto

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,6 +1,30 @@
-const createProject = (req, res) => {
+/* Importaciones propias */
+const Project = require('../models/Project');
+
+const createProject = async (req, res) => {
+    const {name} = req.body;
+    const {_id} = req.user;
+
+    try {
+        const project = new Project({name, creator: _id});
+
+        /* Guardar en DB */
+        await project.save();
+
+        res.status(201).json({
+            ok: true,
+            project
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el Administrador'
+        });
+    }
+
     res.status(201).json({
-       ok: true
+        ok: true
     });
 }
 
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -8,11 +8,19 @@ const {check} = require('express-validator');
 
 /* Importaciones propias */
 const {createProject} = require('../controllers/projects');
+const {validateFields} = require('../middlewares/validate-fields');
+const {validateJwt} = require('../middlewares/validate-jwt');
 
 /* Configuración de Router */
 const router = Router();
 
+/* Validación de proyectos para todas las rutas de Proyectos */
+router.use(validateJwt);
+
 /* Crear nuevo proyecto */
-router.post('/', [], createProject);
+router.post('/', [
+    check('name', 'El nombre es obligatorio').notEmpty(),
+    validateFields
+], createProject);
 
 module.exports = router;
